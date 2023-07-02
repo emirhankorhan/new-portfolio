116965a8c741a13c1f3b4ba5c07e4f08134e066d
emirhankorhan <korhanemirhann@gmail.com>
2023-07-02 22:51:13 +0300

    three-commit

--- src/App.js
+++ src/App.js
@@ -17,87 +17,89 @@ function App() {
   if (degis === true) {
     return (
       <div className='genel-div'>
-        
-        
-      <div className='body-div'>
-        <ul class="navigation">
-      <li class="navigation__item">
-        <a class="navigation__link" href="/home" data-text="Home">Home</a>
-      </li>
-      <li class="navigation__item">
-        <a class="navigation__link" href="/about" data-text="About">About</a> 
-      </li>
-      <li class="navigation__item">
-        <a class="navigation__link" href="/portfolio" data-text="Portfolio">Portfolio</a>  
-      </li>
-      <li class="navigation__item">
-        <a class="navigation__link" href="/contact" data-text="Contact">Contact</a> 
-      </li>
-    </ul></div>
-    <div className='kısa-bilgi'>
-      <div className='kısa-bos'></div>
-      <div className='kısa-yazi'>Copyright © 2023, All Rights Reserved.</div>
-      <div className='kısa-button'>
-        <div className='dark-mode'>
-        {degis ? <button onClick={degisButton} className='moon-button'>
-          {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
-            
-            </button> : <button onClick={degisButton2} className='sun-button'>
-          {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
-            
-            </button>}
-          
-            
+        <div className='kısa-bilgi'>
+          <div className='kısa-bos'></div>
+          <div className='kısa-yazi'>Copyright © 2023, All Rights Reserved.</div>
+          <div className='kısa-button'>
+            <div className='dark-mode'>
+              {degis ? <button onClick={degisButton} className='moon-button'>
+                {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
+
+              </button> : <button onClick={degisButton2} className='sun-button'>
+                {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
+
+              </button>}
+
+
+            </div>
+          </div>
         </div>
+
+
+        <div className='body-div'>
+          <ul class="navigation">
+            <li class="navigation__item">
+              <a class="navigation__link" href="/home" data-text="Home">Home</a>
+            </li>
+            <li class="navigation__item">
+              <a class="navigation__link" href="/about" data-text="About">About</a>
+            </li>
+            <li class="navigation__item">
+              <a class="navigation__link" href="/portfolio" data-text="Portfolio">Portfolio</a>
+            </li>
+            <li class="navigation__item">
+              <a class="navigation__link" href="/contact" data-text="Contact">Contact</a>
+            </li>
+          </ul></div>
+
       </div>
-      </div>
-    </div>
     );
-    
-  }else{
+
+  } else {
     return (
       <div className='genel-div'>
-        
-        
-      <div className='body-div-dark'>
-        <ul class="navigation-dark">
-      <li class="navigation__item-dark">
-        <a class="navigation__link-dark" href="/home" data-text="Home">Home</a>
-      </li>
-      <li class="navigation__item-dark">
-        <a class="navigation__link-dark" href="/about" data-text="About">About</a> 
-      </li>
-      <li class="navigation__item-dark">
-        <a class="navigation__link-dark" href="/portfolio" data-text="Portfolio">Portfolio</a>  
-      </li>
-      <li class="navigation__item-dark">
-        <a class="navigation__link-dark" href="/contact" data-text="Contact">Contact</a> 
-      </li>
-    </ul></div>
-    <div className='kısa-bilgi-dark'>
-      <div className='kısa-bos-dark'></div>
-      <div className='kısa-yazi-dark'>Copyright © 2023, All Rights Reserved.</div>
-      <div className='kısa-button-dark'>
-        <div className='dark-mode-dark'>
-        {degis ? <button onClick={degisButton} className='moon-button-dark'>
-          {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
-            
-            </button> : <button onClick={degisButton2} className='sun-button-dark'>
-          {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
-            
-            </button>}
-          
-            
+        <div className='kısa-bilgi-dark'>
+          <div className='kısa-bos-dark'></div>
+          <div className='kısa-yazi-dark'>Copyright © 2023, All Rights Reserved.</div>
+          <div className='kısa-button-dark'>
+            <div className='dark-mode-dark'>
+              {degis ? <button onClick={degisButton} className='moon-button-dark'>
+                {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
+
+              </button> : <button onClick={degisButton2} className='sun-button-dark'>
+                {degis ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-sun"></i>}
+
+              </button>}
+
+
+            </div>
+          </div>
         </div>
+
+
+        <div className='body-div-dark'>
+          <ul class="navigation-dark">
+            <li class="navigation__item-dark">
+              <a class="navigation__link-dark" href="/home" data-text="Home">Home</a>
+            </li>
+            <li class="navigation__item-dark">
+              <a class="navigation__link-dark" href="/about" data-text="About">About</a>
+            </li>
+            <li class="navigation__item-dark">
+              <a class="navigation__link-dark" href="/portfolio" data-text="Portfolio">Portfolio</a>
+            </li>
+            <li class="navigation__item-dark">
+              <a class="navigation__link-dark" href="/contact" data-text="Contact">Contact</a>
+            </li>
+          </ul></div>
+
       </div>
-      </div>
-    </div>
     );
-    
+
   }
 
 
-  
+
 }
 
 export default App;
